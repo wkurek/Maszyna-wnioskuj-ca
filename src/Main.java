a6d93959cce04b5b68bb50dce619eaa22d89a8bd
wkurek <w.kurek2@gmail.com>
2019-01-20 13:51:50 +0100

    KnowledgeBaseExtender

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ import util.printer.ResultPrinter;
 import java.io.IOException;
 import java.util.List;
 
+import static util.parser.KnowledgeBaseExtender.extendKnowledgeBase;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -58,36 +60,5 @@ public class Main {
             parameterException.getJCommander().usage();
         }
     }
-
-    private static void extendKnowledgeBase(ClausureSet knowledgeBase, List<Constant> constants) {//A=>B, więc trzeba dodać -B=>-A
-        int end = knowledgeBase.getClousuresCount();
-        for(int i = 0; i<end; i++)
-        {
-            if(knowledgeBase.getClausures(i).hasPremise())
-            {
-                ClausureSet allExtensions = knowledgeBase.getClausures(i).generateExtensions(constants);
-                for(int k = 0; k<allExtensions.getClousuresCount(); k++)
-                {
-                    if(!existsExtendedClausure(knowledgeBase, allExtensions.getClausures(k)))
-                    {
-                        knowledgeBase.add(allExtensions.getClausures(k));
-
-                    }//there is no Clausure like the generated one yet
-                }//for through all combinations
-
-            }//if hasPremise
-        }//for knowledgeSet
-    }
-
-    private static boolean existsExtendedClausure(ClausureSet knowledgeBase, Clausure clausure) {
-        for(int i = 0; i<knowledgeBase.getClousuresCount(); i++)
-        {
-            if(clausure.sameAs(knowledgeBase.getClausures(i)))
-                return true;
-        }
-        return false;
-    }
-
-
 }
 
